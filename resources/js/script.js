b24dcb4b12912c641bdd23ecae585bae4469ce8e
Vasilii Fadeev <vfadeev89@mail.ru>
2019-03-31 20:48:44 +0300

    Реализована навигация по секциям в виде плавного скролла к нужному элементу

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,12 @@
+const scrollSpeed = 1000;
+
 $(document).ready(function () {
+    setupStickyNavigation();
+    setupScrollOnButtons();
+    setupScrollOnAnchors();
+});
+
+function setupStickyNavigation() {
     $('.js--section-features').waypoint(function (direction) {
         if (direction === 'down') {
             $('nav').addClass('sticky');
@@ -8,4 +16,31 @@ $(document).ready(function () {
     }, {
         offset: '60px'
     });
-});
+}
+
+function setupScrollOnButtons() {
+    $('.js--scroll-to-plans').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, scrollSpeed);
+    });
+    $('.js--scroll-to-start').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, scrollSpeed);
+    });
+}
+
+function setupScrollOnAnchors() {
+    $('a[href^="#"]').click(function (event) {
+        let target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, scrollSpeed);
+        }
+    });
+}
